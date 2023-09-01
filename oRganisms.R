0603161809a410d949cba8fae8b0fa91e8d0a91b
ahoog11 <143419359+ahoog11@users.noreply.github.com>
2023-09-01 10:50:57 -0400

    commit 4

--- oRganisms.R
+++ oRganisms.R
@@ -62,3 +62,12 @@ for(i in species){
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
 }
+
+pdf("species_quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
