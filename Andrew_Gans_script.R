0733ab0fb4f442af06a77d991d57aa174e6e0744
Andrew Gans <andrewgans@andrews-mbp-75.bc.edu>
2023-09-08 10:41:36 -0400

    Final Commit 8Sep23

--- Andrew_Gans_script.R
+++ Andrew_Gans_script.R
@@ -34,7 +34,7 @@ for(i in species){
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
 }
-pdf("Andrew_Gans_species_quadrant_test.pdf")
+pdf("Andrew_Gans_species_quadrant.pdf")
 for(i in species){
   p <- dat %>%
     filter(species==i) %>%
@@ -42,4 +42,4 @@ for(i in species){
   print(p)
 }
 dev.off()
-
+list.files(pattern = ".pdf")
