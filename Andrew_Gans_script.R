8e50c86f93d6f3d8eb9451550569ac36bf75a672
Andrew Gans <andrewgans@andrews-mbp-75.bc.edu>
2023-09-08 11:26:00 -0400

    Final Gans Project 1 Commit

--- Andrew_Gans_script.R
+++ Andrew_Gans_script.R
@@ -3,29 +3,27 @@
 library(ggplot2)
 library(tidyverse)
 #working directory already set manually
+##Question 1 - A dat variable containing the scales dataset##
 #load dataset
 dat <- read.csv("scales.csv")
-#Dataf summary
-dim(dat)
-head(dat)
-#Data classes + operations
+##Question 2 - A line of code which reports the class of each column in the dataset##
 sapply(dat,class)
-#Constructing summary dataframe for species count in dataset
-dat$species <- as.factor(dat$species)
-species <- levels(dat$species)
-species
-length(species)
-##Number of Punctures for each Species
+##Question 3 - A line of code which reports the dimensions of the dataset##
+dim(dat)
+###Question 4 - Code that produces a summary of the number of scales punctured for each species##
+#Number of Punctures for each Species
 species.n <- dat %>%
   group_by(species) %>%
   summarise(n = n())
 species.n
-##Number of Specimens Sampled For Each Species
+##Question 5 - Code that produces a summary of the number of specimens sampled for each species##
+#Number of Specimens Sampled For Each Species
 dat %>% 
   count(species,specimen) %>%
   print() %>%
   count(species,name = "n.specimens")
-##Compile PDF
+##Question 6 - Code that produces a PDF file containing 6 figures##
+#Compile PDF
 for(i in species){
   p <- dat %>%
     filter(species==i)%>%
