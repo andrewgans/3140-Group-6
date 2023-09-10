85e970303dd01b48653868340cc9858297e0bf18
ahoog11 <143419359+ahoog11@users.noreply.github.com>
2023-09-10 12:55:03 -0400

    commit 34

--- Alexandra_Hoogendijk_script.R
+++ Alexandra_Hoogendijk_script.R
@@ -14,7 +14,7 @@ dim(dat)
 #Question 2: A line of code which reports the class of each column in the dataset.
 sapply(dat,class)
 
-# Question 4: Code that produces a summary of the number of scales punctured for each species.
+# : Code that produces a summary of the number of scales  for each species.
 dat %>%
   group_by(species) %>%
   summarise(n = n())
@@ -25,7 +25,7 @@ species.n<- dat %>%
   summarise(n = n())
 species.n
 
-#Code that produces a summary of the number of scales punctured for each species.
+#Question 4: Code that produces a summary of the number of scales punctured for each species.
 dat %>% 
   count(species,specimen) %>%
   print() %>%
